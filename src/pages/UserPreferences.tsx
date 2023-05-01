735492df6d925d9d64e67af8f23573645bb26567
KimbohLovette <kimbohlovette@gmail.com>
2023-05-01 03:18:08 +0100

    Android ripples stop at border fixed

--- src/pages/UserPreferences.tsx
+++ src/pages/UserPreferences.tsx
@@ -1,10 +1,16 @@
 import { View, Text, Pressable } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const UserPreferences = () => {
   return (
-    <View className="w-full">
-      <Text className="text-center text-2xl font-semibold mb-8">WhoGet</Text>
+    <View className="relative w-full">
+      <Pressable
+        android_ripple={{ color: 'slate', borderless: true }}
+        className="absolute right-4 top-2 rounded-md overflow-hidden">
+        <Icon name="close-outline" size={25} />
+      </Pressable>
+      <Text className="text-center text-2xl font-semibold my-8">WhoGet</Text>
       <View className="py-2">
         <Text className="text-center font-semibold text-slate-800">
           Explore the best of WhoGet
@@ -54,17 +60,19 @@ const CatBtn = (props: { name: string; selected?: boolean }) => {
     setSelected(select => !select);
   };
   return (
-    <Pressable
-      android_ripple={{ color: 'slate' }}
-      onPress={toggleSelect}
-      className="relative items-center py-2 px-2 border border-slate-300 rounded-md text-medium">
-      <Text className="">{props.name}</Text>
-      {selected && (
-        <View className="absolute rounded-full bg-orange-400 w-5 aspect-square justify-center items-center -top-1 -right-1">
-          <Text className="text-[10px] text-white">25</Text>
-        </View>
-      )}
-    </Pressable>
+    <View className="relative items-center border border-slate-300 rounded-md overflow-hidden">
+      <Pressable
+        android_ripple={{ color: 'slate' }}
+        onPress={toggleSelect}
+        className="py-2 px-2 ">
+        <Text className="">{props.name}</Text>
+        {selected && (
+          <View className="absolute rounded-full bg-orange-400 w-5 aspect-square justify-center items-center -top-1 -right-1">
+            <Text className="text-[10px] text-white">25</Text>
+          </View>
+        )}
+      </Pressable>
+    </View>
   );
 };
 export default UserPreferences;
