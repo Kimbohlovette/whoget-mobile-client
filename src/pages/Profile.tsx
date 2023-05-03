4ce1dbf3fbe0dfb9380b0989224b9174a1dc3258
KimbohLovette <kimbohlovette@gmail.com>
2023-05-03 05:22:05 +0100

    User details page complete

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,8 @@
 import { View, Text, Pressable, TextInput } from 'react-native';
 import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { textEllipsis } from '../shared/ellipseText';
+import { ScrollView } from 'react-native';
 
 const Profile = () => {
   const [editable, setEditable] = useState(false);
@@ -59,9 +61,43 @@ const Profile = () => {
         </View>
       </View>
       <View className="w-full">
-        <View>
+        <View className="relative py-1 w-1/5">
           <Text className="text-slate-700">My Asks</Text>
+          <View className="absolute top-0 right-1 h-4 aspect-square justify-center items-center bg-secondary-500 rounded-full">
+            <Text className="text-[10px] text-white">2</Text>
+          </View>
         </View>
+        <ScrollView className="w-full gap-y-2 divide-y divide-slate-200">
+          <View className="">
+            <UserAsk />
+          </View>
+          <View>
+            <UserAsk />
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+const UserAsk = () => {
+  return (
+    <View className="flex-row items-center gap-x-2 justify-between py-2">
+      <View className="flex-1 flex-row items-center">
+        <View className="h-12 mr-2 aspect-square bg-slate-300 rounded-full">
+          <Text> </Text>
+        </View>
+        <View className="flex-1">
+          <Text className="text-slate-500">
+            {textEllipsis(
+              'Description sfkdf sdidre fleow foere n iereisk ierjid isodr ofie oher',
+              10,
+            )}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <Text className="text-slate-400 text-sm">Yesterday</Text>
       </View>
     </View>
   );
