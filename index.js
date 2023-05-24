cadb747a4117550187bb1d3848c477d0fbe3bb4f
KimbohLovette <kimbohlovette@gmail.com>
2023-05-24 10:20:45 +0100

    Installed pusher-react-native-websocket

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ import { name as appName } from './app.json';
 import { store } from './src/store/store';
 import { Provider } from 'react-redux';
 import React from 'react';
+import { MenuProvider } from 'react-native-popup-menu';
 
 AppRegistry.registerComponent(appName, () => () => (
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <MenuProvider>
+        <App />
+      </MenuProvider>
     </Provider>
   </React.StrictMode>
 ));
