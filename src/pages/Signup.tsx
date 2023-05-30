35b89b5b803e64e384a6529c617f8286211169c7
KimbohLovette <kimbohlovette@gmail.com>
2023-05-30 16:34:08 +0100

    Adjusted button sizing and typography on signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -27,20 +27,20 @@ const Signup = () => {
           className="h-28 aspect-video"
         />
       </View>
-      <View className="w-5/6 mx-auto bg-white p-5">
-        <Text className="my-8 text-lg text-center text-slate-700 font-semibold">
+      <View className="bg-white p-5">
+        <Text className="my-8 text-xl text-center text-slate-700 font-semibold">
           Signup to join WhoGet
         </Text>
-        <View className="gap-y-4 justify-center items-center">
+        <View className="gap-y-8 justify-center items-center">
           <Pressable
             disabled
             android_ripple={{ color: 'light-gray' }}
-            className="w-full py-2 px-5 flex-row items-center justify-start border border-slate-300 rounded-md">
+            className="w-full py-3 px-5 flex-row items-center justify-start border border-slate-300 rounded-xl">
             <Image
               source={require('../assets/facebook.png')}
               className="mr-4"
             />
-            <Text>Continue with Facebook</Text>
+            <Text className="text-base">Continue with Facebook</Text>
           </Pressable>
           <Pressable
             onPress={() => {
@@ -69,24 +69,24 @@ const Signup = () => {
               });
             }}
             android_ripple={{ color: 'light-gray' }}
-            className="w-full py-2 px-5 flex-row items-center justify-start border border-slate-300 rounded-md">
+            className="w-full py-3 px-5 flex-row items-center justify-start border border-slate-300 rounded-xl">
             <Image source={require('../assets/google.png')} className="mr-4" />
-            <Text>Continue with Google</Text>
+            <Text className="text-base">Continue with Google</Text>
           </Pressable>
           {authMessge === 'account_exists' && (
-            <Text className="text-amber-700 text-center">
+            <Text className="text-amber-700 text-center text-lg">
               Account already exists. Redirecting to signin...
             </Text>
           )}
           {authMessge === 'signup_successful' && (
-            <Text className="text-amber-700 text-center">
+            <Text className="text-amber-700 text-center text-sm">
               Successfully signed up. Redirecting to home page...
             </Text>
           )}
         </View>
         <View className="py-8 gap-y-5">
           <View className="flex-row gap-1 items-center justify-center">
-            <Text className="text-center text-slate-600">
+            <Text className="text-center text-slate-600 text-base">
               Already have an account?
             </Text>
 
@@ -95,16 +95,16 @@ const Signup = () => {
                 onPress={() => {
                   navigation.replace('Signin');
                 }}>
-                <Text className="text-orange-400">Sign in</Text>
+                <Text className="text-orange-400 text-base">Sign in</Text>
               </Pressable>
             </View>
           </View>
-          <View>
-            <Text className="text-center text-xs">
+          <View className="mt-5">
+            <Text className="text-center text-ms">
               By clicking signup, you agree to our
             </Text>
             <Pressable>
-              <Text className="text-xs text-center py-1 text-blue-400">
+              <Text className="text-sm text-center py-1 text-blue-400">
                 Terms of service and privacy policy.
               </Text>
             </Pressable>
